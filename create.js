b220d52008d0cbeaef963308a088767ecd426566
dung1993 <crorstar1704@gmail.com>
2023-01-11 14:03:42 +0700

    singlePage

---
+++ create.js
@@ -0,0 +1,23 @@
+function makeEdit() {
+    document.querySelector(".model-container").classList.add('show');
+
+}
+
+function closeAdd() {
+    document.querySelector(".model-container").classList.remove('show');
+  
+}
+
+function makeCancel() {
+    document.querySelector(".model-container").classList.remove('show');
+ 
+}
+
+function makeDelete(){
+    document.querySelector(".model-delete").classList.add('show');
+   
+}
+function closeDelete(){
+    document.querySelector(".model-delete").classList.remove("show");
+
+}
